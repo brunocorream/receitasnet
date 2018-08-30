aece670dec23c240bb4ea272468e09f4ec4129ff
Bruno Martins <brunocorream@gmail.com>
2018-08-30 12:50:40 -0300

    Configuração da rota recipe, ajustes do gemfile para development e production mantendo o sqlite apenas para testes e usando PG para production

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,7 @@
+class RecipesController < ApplicationController
+    
+    def index
+    @recipes = ['Strogonofe', 'File de Frango a Parmegiana', 'Salmão ao Forno', 'Feijoada Mineira']
+    end
+    
+end
