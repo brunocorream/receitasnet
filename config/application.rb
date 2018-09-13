7e7725b0a479d20fa0f73a52a7d5e0bd0d446042
Bruno Martins <brunocorream@gmail.com>
2018-09-12 22:45:20 -0300

    Internalização do idioma

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,10 @@ module Receitalistanet
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Brasilia'
+
+    config.i18n.default_locale = "pt-BR"
+    I18n.config.available_locales = "pt-BR"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
