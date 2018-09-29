f2aab358dfbb86d2519b2759b6557dde2e3ebb4f
Bruno Martins <brunocorream@gmail.com>
2018-09-28 21:30:09 -0300

    Exibindo os comentários e atualizando os valores para real brazileiro

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ApplicationRecord
-  validades :name, :rating, :comment, :presence:true
-  validate_lenght_of :comment, maximum:200
-  RATING=[0,1,2,3,4,5]
-  validates_inclusion_of :rating in: RATING
+  validates :name, :rating, :comment, presence: true
+  validates_length_of :comment, maximum: 50
+
+  RATING = [0,1,2,3,4,5]
+  validates_inclusion_of :rating, in: RATING
   belongs_to :recipe
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :comments
   root 'recipes#index' #para que o heroku reconheça a página inicial
-  resources :recipes 
+  resources :recipes do
+    resources :comments
+  end
 end
